cf8dbc6297f1c29e463f5cdf78f9b6b4069a8d7e
srobon20 <srobona.dey20@gmail.com>
2022-08-17 18:45:06 +0530

    login and register is done

--- server.js
+++ server.js
@@ -10,13 +10,26 @@ const PORT=process.env.PORT||3000;
 
 app.use(express.static('public'))
 
+app.use(expressLayout);
+app.set('views',path.join(__dirname,'/resources/views'))
+app.set('view engine','ejs');
+
 app.get('/',(req,res)=>{
     res.render('home')
 })
 
-app.use(expressLayout);
-app.set('views',path.join(__dirname,'/resources/views'))
-app.set('view engine','ejs');
+app.get('/cart',(req,res)=>{
+    res.render('customers/cart')
+})
+
+app.get('/register',(req,res)=>{
+    res.render('auth/register');
+})
+
+app.get('/login',(req,res)=>{
+    res.render('auth/login');
+})
+
 
 app.listen(PORT,()=>{
     console.log(`server is running on ${PORT}`)
